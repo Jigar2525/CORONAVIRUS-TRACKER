e2d90ef0db18b540892b5b67797c93a21ead26dd
Jigar2525 <rathodjigar2511@gmail.com>
2023-11-17 13:54:37 +0530

    A new project is added

---
+++ OneDrive/Desktop/CORONAVIRUS TRACKER/my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Covid } from './Components/covid'
+
+export const App = () => {
+  return (
+    <React.Fragment>
+      <Covid/>
+    </React.Fragment>
+  )
+}
+
